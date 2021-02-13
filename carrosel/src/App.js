ee1599639ec0f86a934d88574289ccbed21d9ba6
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2021-02-13 08:14:05 -0300

    car

---
+++ carrosel/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+
+import Carrosel1 from './componentes/Carrosel1'
+import Fotos from './componentes/Fotos'
+
+
+function App() {
+  return (
+   
+      <div className="App">
+     
+
+        <Fotos/>
+
+
+
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ carrosel/src/componentes/Carrosel1.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import Img from 'react-image'
+import Teacher1 from "./img/imagem1.jpeg"
+import Teacher2 from "./img/imagem2.jpeg"
+import Teacher3 from "./img/imagem3.jpeg"
+
+const Carrosel1 = props => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-8 m-auto">
+
+                        <h1>Slider simples</h1>
+
+                        <div className="carousel slide" data-ride="carousel">
+
+                            <div className="carousel-inner">
+
+                                <div className="carousel-item ">
+                                    <img src={Teacher1} className="img-fluid" />
+                                </div>
+                                <div className="carousel-item active">
+                                    <img src={Teacher2} className="img-fluid" />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src={Teacher3} className="img-fluid" />
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                        <h1>Slider com controles</h1>
+                        <div id="carousel-controles" class="carousel slide" data-ride="carousel">
+
+                            <div class="carousel-inner">
+
+                                <div className="carousel-item ">
+                                    <img src={Teacher1} className="img-fluid" />
+                                </div>
+                                <div className="carousel-item active">
+                                    <img src={Teacher2} className="img-fluid" />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src={Teacher3} className="img-fluid" />
+                                </div>
+                            </div>
+
+                            
+
+                            <a href="#carousel-controles" className="carousel-control-prev" data-slide="prev">
+                                <samp className="carousel-control-prev-icon"></samp>
+                            </a>
+                            <a href="#carousel-controles" className="carousel-control-next" data-slide="next">
+                                <samp className="carousel-control-next-icon"></samp>
+                            </a>
+
+                        </div>
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Carrosel1
